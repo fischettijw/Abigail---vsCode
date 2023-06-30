93da55bee402f1f05b95135750551c170353fe2a
Joseph Fischetti <fischettijw@gmail.com>
2023-06-30 08:10:04 -0400

    created new branch  063023-08-09AM

---
+++ Planets/Abigail/sandbox.py
@@ -0,0 +1,24 @@
+import os; os.system('cls')
+
+text = 'AbCdEf'
+
+print(f'text = {text}')
+print(f'text = {text:<20}')
+print(f'text = {text:>20}')
+print(f'text = {text:^20}')
+print('-'*30)
+print(f'text = {text:~<20}')
+print(f'text = {text:#>20}')
+print(f'text = {text:*^20}')
+print('-'*30)
+
+
+flt = 123456789.123456789
+print(f'flt = {flt:.4f}')
+print(f'flt = {flt:.4e}')
+print()
+print()
+
+
+
+
